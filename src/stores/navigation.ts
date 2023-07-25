a605127b73bfb57dc4bc46d792a2f3a3e64ed1d4
wirelessEye <okuloj@outlook.com>
2023-07-25 13:21:10 +1000

    useNavigation: add history

--- src/stores/navigation.ts
+++ src/stores/navigation.ts
@@ -10,10 +10,15 @@ type ScreenID = keyof typeof screens;
 interface NavigationProps {
     Screen: () => JSX.Element;
     push: (id: ScreenID) => void;
+    history: ScreenID[];
 }
 
 export const useNavigation = create<NavigationProps>((set) => ({
     Screen: screens.welcome,
-    push: (id) => set({ Screen: screens[id] }),
+    push: (id) =>
+        set((state) => ({
+            Screen: screens[id],
+            history: [...state.history, id],
+        })),
+    history: [],
 }));
-
